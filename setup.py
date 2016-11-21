71aab00a553ba2f5d5dc6117914451dd88c4a0c0
lindsay <techells22@gmail.com>
2016-11-21 10:18:28 -0600

    update setup file

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@ from setuptools import setup, find_packages
 
 print find_packages()
 setup(name='python_read_file',
-      version='0.0.1',
-      description='read a file | convert list values to dict',
-      author='lynzt',
-      url='https://github.com/lynzt/python_read_file',
-      packages=['read_file'],
-      install_requires=[
-          'csv',
-      ],
-     )
+    version='0.0.1',
+    description='read a file | convert list values to dict',
+    author='lynzt',
+    url='https://github.com/lynzt/python_read_file',
+    packages=['read_file'],
+    )
