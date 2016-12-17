75e7516a808fea94c20b1fe1486ea1df93b90737
lindsay <techells22@gmail.com>
2016-12-17 14:57:22 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 print find_packages()
 setup(name='python_read_file_generator',
-    version='0.0.1',
+    version='0.0.2',
     description='read a file | convert row/list values to dict',
     author='lynzt',
     url='https://github.com/lynzt/python_read_file_generator',
