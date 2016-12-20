d9e5a1944482efce75fe32604830b8cb028a5518
lindsay <techells22@gmail.com>
2016-12-19 22:18:27 -0600

    remove print stmt after upgrade to python 3

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-print find_packages()
 setup(name='python_read_file_generator',
     version='0.0.2',
     description='read a file | convert row/list values to dict',
